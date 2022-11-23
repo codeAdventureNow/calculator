f993089537e5bfddf3953213492ac03e24ab858f
[David <[nmueller855@gmail.com]>
2022-11-23 09:45:09 -0800

    experimenting with how the call back function of the event listener will operate

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Number
 const buttonZero = document.querySelector(".button-zero");
 const buttonOne = document.querySelector(".button-one");
 const buttonTwo = document.querySelector(".button-two");
@@ -9,34 +10,61 @@ const buttonSeven = document.querySelector(".button-seven");
 const buttonEight = document.querySelector(".button-eight");
 const buttonNine = document.querySelector(".button-nine");
 
-const zero = Math.floor(buttonZero.value);
-const one = Math.floor(buttonOne.value);
-const two = Math.floor(buttonTwo.value);
-const three = Math.floor(buttonThree.value);
-const four = Math.floor(buttonFour.value);
-const five = Math.floor(buttonFive.value);
-const six = Math.floor(buttonSix.value);
-const seven = Math.floor(buttonSeven.value);
-const eight = Math.floor(buttonEight.value);
-const nine = Math.floor(buttonNine.value);
+//Other buttons
+const buttonDecimal = document.querySelector(".button-decimal");
+const buttonAllClear = document.querySelector(".all-clear");
+
+//Operands
+const buttonAdd = document.querySelector(".button-add");
+const buttonSubtract = document.querySelector(".button-subtract");
+const buttonDivide = document.querySelector(".button-divide");
+const buttonMultiply = document.querySelector(".button-multiply");
+const buttonEqual = document.querySelector(".button-equal");
+
+// const zero = 0;
+const one = parseFloat(buttonOne.value);
+const two = parseFloat(buttonTwo.value);
+// const three = parseFloat(buttonThree.value);
+// const four = parseFloat(buttonFour.value);
+// const five = parseFloat(buttonFive.value);
+// const six = parseFloat(buttonSix.value);
+// const seven = parseFloat(buttonSeven.value);
+// const eight = parseFloat(buttonEight.value);
+// const nine = parseFloat(buttonNine.value);
+
+//How do I convert this from a string into an actual decimal?
+const decimal = buttonDecimal.value;
+
+console.log(typeof decimal);
 
 function printZero() {
-  console.log(zero);
-  return zero;
+  console.log(0);
+  return 0;
 }
 
+// function printOne() {
+//   console.log(1);
+//   return 1;
+// }
+
+// function printTwo() {
+//   console.log(2);
+//   return 2;
+// }
+
 function printOne() {
-  console.log(one);
   return one;
 }
 
 function printTwo() {
-  console.log(two);
+  return two;
 }
 
 function add(a, b) {
   return a + b;
 }
+
+console.log(add(printOne, printTwo));
 
 function printNumber(e) {}
 
