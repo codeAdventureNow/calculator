1500c57a4ea2e60860607260ead695d023bb08c6
[David <[nmueller855@gmail.com]>
2022-11-29 08:26:30 -0800

    working on second version of calculator, scrapped the old version of JS

--- script.js
+++ script.js
@@ -1,80 +1,35 @@
-//Number
-const buttonZero = document.querySelector(".button-zero");
-const buttonOne = document.querySelector(".button-one");
-const buttonTwo = document.querySelector(".button-two");
-const buttonThree = document.querySelector(".button-three");
-const buttonFour = document.querySelector(".button-four");
-const buttonFive = document.querySelector(".button-five");
-const buttonSix = document.querySelector(".button-six");
-const buttonSeven = document.querySelector(".button-seven");
-const buttonEight = document.querySelector(".button-eight");
-const buttonNine = document.querySelector(".button-nine");
+const buttons = document.querySelectorAll("button");
 
-//Other buttons
-const buttonDecimal = document.querySelector(".button-decimal");
-const buttonAllClear = document.querySelector(".all-clear");
+//If I do it this way how will I handle operand click?
 
-//Operands
-const buttonAdd = document.querySelector(".button-add");
-const buttonSubtract = document.querySelector(".button-subtract");
-const buttonDivide = document.querySelector(".button-divide");
-const buttonMultiply = document.querySelector(".button-multiply");
-const buttonEqual = document.querySelector(".button-equal");
+//How do I convert operand strings into acutal operands?
 
-// const zero = 0;
-const one = parseFloat(buttonOne.value);
-const two = parseFloat(buttonTwo.value);
-// const three = parseFloat(buttonThree.value);
-// const four = parseFloat(buttonFour.value);
-// const five = parseFloat(buttonFive.value);
-// const six = parseFloat(buttonSix.value);
-// const seven = parseFloat(buttonSeven.value);
-// const eight = parseFloat(buttonEight.value);
-// const nine = parseFloat(buttonNine.value);
+//How do I put the number in the screen dispplay
 
-//How do I convert this from a string into an actual decimal?
-const decimal = buttonDecimal.value;
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
-console.log(typeof decimal);
+function handleButtonClick(event) {
+  const button = event.currentTarget;
+  const value = button.value;
+  const number = Number(value);
 
-function printZero() {
-  console.log(0);
-  return 0;
+  value = calculator.displayValue;
+  console.log(number);
+  console.log(typeof number);
 }
 
-// function printOne() {
-//   console.log(1);
-//   return 1;
-// }
-
-// function printTwo() {
-//   console.log(2);
-//   return 2;
-// }
-
-function printOne() {
-  return one;
+function updateDisplay() {
+  const display = document.querySelector(".calc-display");
+  display.value = calculator.displayValue;
 }
 
-function printTwo() {
-  return two;
-}
+updateDisplay();
 
-function add(a, b) {
-  return a + b;
-}
-
-console.log(add(printOne, printTwo));
-
-function printNumber(e) {}
-
-buttonZero.addEventListener("click", printZero);
-buttonOne.addEventListener("click", printOne);
-buttonTwo.addEventListener("click", printTwo);
-// buttonThree.addEventListener("click", printThree);
-// buttonFour.addEventListener("click", printFour);
-// buttonFive.addEventListener("click", printFive);
-// buttonSix.addEventListener("click", printSix);
-// buttonSeven.addEventListener("click", printSeven);
-// buttonEight.addEventListener("click", printEight);
-// buttonNine.addEventListener("click", printNine);
+buttons.forEach((button) =>
+  button.addEventListener("click", handleButtonClick)
+);
