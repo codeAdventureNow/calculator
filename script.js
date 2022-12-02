1ea8193af77e62bad9a86ce4f87350395a8d7150
[David <[nmueller855@gmail.com]>
2022-12-02 08:06:21 -0800

    calculator working on equal sign, trying to figure out how to get the second calculation to replace the first one

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-const buttons = document.querySelectorAll("button");
-const display = document.querySelector(".calc-display");
-const clear = document.querySelector(".all-clear");
-const equal = document.querySelector(".equal-sign");
+(function () {
+  const buttons = document.querySelectorAll(".button");
+  const display = document.querySelector(".calc-display");
+  const clear = document.querySelector(".all-clear");
+  const equal = document.querySelector(".equal-sign");
 
-function handleButtonClick(event) {
-  let value = event.target.dataset.num;
-  display.value += value;
-}
+  // display.value = "0";
 
-buttons.forEach((button) =>
-  button.addEventListener("click", handleButtonClick)
-);
-
-equal.addEventListener("click", function () {
-  if (display.value === "") {
-    display.value = "";
-  } else {
-    let answer = eval(display.value);
-    display.value = answer;
-    console.log(answer);
+  function handleButtonClick(event) {
+    let value = event.target.dataset.num;
+    console.log(value);
+    display.value += value;
   }
-});
 
-clear.addEventListener("click", function () {
-  display.value = "0";
-});
+  buttons.forEach((button) =>
+    button.addEventListener("click", handleButtonClick)
+  );
+
+  equal.addEventListener("click", function (e) {
+    if (display.value === "") {
+      display.value = "";
+    } else {
+      let answer = eval(display.value);
+      display.value = answer;
+    }
+  });
+
+  clear.addEventListener("click", function () {
+    display.value = "";
+  });
+})();
+
+// function updateDisplay(value) {
+//   display.value = "0";
+// }
+
+// updateDisplay();
+
+// display.value = "0";
+
+// function handleButtonClick(event) {
+//   if ((display.value = "0")) {
+//     display.value = "";
+//     let value = event.target.dataset.num;
+//     console.log(value);
+//     display.value += value;
+//   }
+// }
