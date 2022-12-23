2223c1fc3253452e544470e711c5d9cb40d74e2f
[David <[nmueller855@gmail.com]>
2022-12-23 11:46:23 -0800

    decimal is not repeating

--- script.js
+++ script.js
@@ -9,15 +9,19 @@
 
   display.value = '0';
 
+  // function clearPriorCalulation() {
+  //   console.log('What up?');
+  // }
+
   function handleNumberClick(event) {
-    let value = event.target.dataset.num; // 2
+    let value = event.target.dataset.num;
+
+    let lastValue = display.value.charAt(value.length);
 
     if (display.value === '0') {
       // what about if the value is a . ?
       display.value = '' + value;
-    }
-    // for any number click after the first one the 2nd after an operator click
-    else {
+    } else {
       display.value += value;
     }
   }
@@ -40,56 +44,11 @@
 
   function handleDecimalClick(event) {
     let value = event.target.dataset.num;
-    display.value += value;
-  }
+    let lastValue = display.value.charAt(value.length);
 
-  function handleButtonClick(event) {
-    let value = event.target.dataset.num; // 2
-
-    let priorClick = display.value.charAt(value.length - 1); // +
-    // console.log(priorClick);
-    // display.value += value;
-
-    // for the first button when the calculator defaults to 0
-    if (display.value === '0') {
-      // what about if the value is a . ?
-      display.value = '' + value;
-    }
-    // for after clicking an operator button
-    else if (
-      priorClick == '+' ||
-      priorClick == '-' ||
-      priorClick == '*' ||
-      priorClick == '/' ||
-      priorClick == '.' // this shouldnt be here
-    ) {
-      // 100
-      // + // priorClick
-      // . // value
-
-      // 1)
-      // if statement
-      // what happens if the button they just clicked is another operator?
-      // 100+
-      // -
-      // 100-
-      // replace the last character with the new operator
-
-      // 2)
-      // else if statement
-      // what happens if the button they just clicked is a number?
-      // just want it to add to end
-      // 100+5
-
-      // 3)
-      // else
-      // what happens if the prior click is an operator e.g. + but the next click is a .
-      // display.value += 0 + value
-
-      return;
-    }
-    // for any number click after the first one the 2nd after an operator click
-    else {
+    if (lastValue == '.') {
+      console.log('Hey buddy');
+    } else {
       display.value += value;
     }
   }
@@ -110,6 +69,7 @@
     } else {
       let answer = eval(display.value);
       display.value = answer;
+      // clearPriorCalulation();
     }
   });
 
@@ -117,3 +77,54 @@
     display.value = '0';
   });
 })();
+
+// function handleButtonClick(event) {
+//   let value = event.target.dataset.num; // 2
+
+//   let priorClick = display.value.charAt(value.length - 1); // +
+//   // console.log(priorClick);
+//   // display.value += value;
+
+//   // for the first button when the calculator defaults to 0
+//   if (display.value === '0') {
+//     // what about if the value is a . ?
+//     display.value = '' + value;
+//   }
+//   // for after clicking an operator button
+//   else if (
+//     priorClick == '+' ||
+//     priorClick == '-' ||
+//     priorClick == '*' ||
+//     priorClick == '/' ||
+//     priorClick == '.' // this shouldnt be here
+//   ) {
+//     // 100
+//     // + // priorClick
+//     // . // value
+
+//     // 1)
+//     // if statement
+//     // what happens if the button they just clicked is another operator?
+//     // 100+
+//     // -
+//     // 100-
+//     // replace the last character with the new operator
+
+//     // 2)
+//     // else if statement
+//     // what happens if the button they just clicked is a number?
+//     // just want it to add to end
+//     // 100+5
+
+//     // 3)
+//     // else
+//     // what happens if the prior click is an operator e.g. + but the next click is a .
+//     // display.value += 0 + value
+
+//     return;
+//   }
+//   // for any number click after the first one the 2nd after an operator click
+//   else {
+//     display.value += value;
+//   }
+// }
